9447bd1b6a4657c981938e1129661750a6619382
Mark Balleby Hansen <mbal@verdo.com>
2020-02-05 08:51:49 +0100

    Created Lambda Expressions!

---
+++ Lambda Expressions/Lambda Expressions/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Lambda_Expressions
+{
+    public delegate int Funcdel1(int a);
+    public delegate float Funcdel2(float a, float b, float c);
+    public delegate string Funcdel3();
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //A)
+            Funcdel1 funcdel1 = (a) => a * a;
+            Console.WriteLine(funcdel1.Invoke(2));
+
+            //B)
+            Funcdel2 funcdel2 = (a, b, c) => a + b + c;
+            Console.WriteLine(funcdel2.Invoke(1,2,3));
+
+            //C)
+            Funcdel3 funcdel3 = () => "Hello World";
+            Console.WriteLine(funcdel3.Invoke());
+        }
+    }
+}
